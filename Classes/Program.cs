96ab081206b9847949cd54ae2d30c7d7c80eabf0
Robin Correa <robin.correa21@gmail.com>
2023-02-28 10:34:37 +0800

    Learn params and out modifier.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -20,9 +20,46 @@ class Program
         Console.WriteLine(customer.Name);
 
         // [Methods]
+
+        // Overloading
         UsePoints();
 
+        // Params modifier
+        UseParams();
 
+        // Out
+
+        // Parse Method
+        try
+        {
+            var num = int.Parse("21");
+            Console.WriteLine(num);
+        }
+        catch (System.Exception)
+        {
+            Console.WriteLine("Conversion failed (Parse).");
+        }
+
+        // TryParse Method (to demo out modifier)
+        int number;
+        var result = int.TryParse("22", out number);
+        if (result)
+        {
+            Console.WriteLine(number);
+        }
+        else
+        {
+            Console.WriteLine("Conversion failed (TryParse).");
+        }
+    }
+    static void UseParams()
+    {
+        // using params modifier to give a method the ability to receive varying number of parameters
+        var calculator = new Calculator();
+        Console.WriteLine(calculator.Add(1, 2));
+        Console.WriteLine(calculator.Add(1, 2, 3));
+        Console.WriteLine(calculator.Add(1, 2, 3, 4));
+        Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
     }
     static void UsePoints()
     {
@@ -36,10 +73,10 @@ class Program
             point.Move(100, 200);
             Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y); // Point is at (100, 200)
         }
-        catch (System.Exception)
+        catch (Exception)
         {
 
-            System.Console.WriteLine("An unexpected error occured.");
+            Console.WriteLine("An unexpected error occured.");
         }
     }
 }
