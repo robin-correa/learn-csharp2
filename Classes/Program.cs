cb89ee704d359e9cafc827697074510f6f109582
Robin Correa <robin.correa21@gmail.com>
2023-02-28 11:10:22 +0800

    Learn Access modifiers in C#.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,13 +1,14 @@
 ﻿namespace Classes;
 using Methods;
 using Fields;
+using AccessModifiers;
 
 class Program
 {
     static void Main(string[] args)
     {
         // [Classes]
-        Console.WriteLine("Classes:");
+        Console.WriteLine("[Classes]");
         // Person class is in Classes/Person.cs
         var person = Person.Parse("Robin");
         person.Introduce("Regina"); // Hi Regina, I am Robin
@@ -22,8 +23,8 @@ class Program
         Console.WriteLine(customer.Name);
 
         // [Methods]
-
-        Console.WriteLine("Methods:");
+        // using Methods;
+        Console.WriteLine("[Methods]");
         // Overloading
         UsePoints();
 
@@ -56,8 +57,9 @@ class Program
         }
 
         // [Fields]
+        // using Fields;
 
-        Console.WriteLine("Fields:");
+        Console.WriteLine("[Fields]");
         var customer2 = new Customer2(1);
         customer2.Orders.Add(new Order());
         customer2.Orders.Add(new Order());
@@ -67,6 +69,24 @@ class Program
         customer2.Promote();
         Console.WriteLine(customer2.Orders.Count); // 0
 
+        // [Access Modifiers]
+        // using AccessModifiers;
+        /*
+            Notes:
+            - In C# we have 5 access modifiers: public, private, protected, internal and protected internal.
+            - A class member declared with public is accessible everywhere.
+            - A class member declared with private is accessible only from inside the class.
+            - We use access modifiers to hide the implementation details of a class. So anything that is about “how” a class does its job should be declared as private. This way, we make sure other parts of the code will not touch the implementation detail of a class. 
+            And as a result we improve the robustness of our code. If change the implementation of a class, we only need to make changes inside the class. No other parts of the code will need to be changed.
+        */
+
+        Console.WriteLine("[Access Modifiers]");
+        var person2 = new Person2();
+        person2.SetBirthdate(new DateTime(1994, 11, 21));
+        Console.WriteLine(person2.GetBirthdate()); // 21/11/1994 00:00:00
+
+        // [Properties]
+        Console.WriteLine("[Properties]");
 
 
     }
